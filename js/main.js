90015fb47f8083b4726dbdc039449bd70ba09796
Ha-im <gungsun1@naver.com>
2025-06-26 18:13:07 +0900

    시네마 제외 전부다 리셋 후 promotion 진행중

--- js/main.js
+++ js/main.js
@@ -4,69 +4,29 @@ document.addEventListener('DOMContentLoaded', function () {
     navigation: true,
     scrollOverflow: true
   });
-  /*
-  // cinema slideToggle
-  const buttons = $('.container.cinema button')
-  buttons.click(function () {
-  $(this).next('div').slideToggle();
-  });
- */
-
-const buttons = $('.container.cinema button')
-const divs = $('.container.cinema button + div')
-const page5 = $('.section.page5')
-const a = $('.cinema a');
-buttons.click(function(){
-  let index = buttons.index(this)
-  console.log(index);
-  buttons.removeClass('active');
-  divs.removeClass('active');
-
-  $(this).eq(0).addClass('active');
-  divs.eq(index).addClass('active');
-
-  if(buttons.eq(0).hasClass('active')){
-    page5.css({
-      backgroundImage:"url('../images/livingroom.png')",
-      backgroundRepeat:"no-repeat",
-      backgroundSize: "cover",
-      backgroundPosition: "center"
+  $('.promotion .store').mouseenter(function(){
+    $(this).find('.hashtag').addClass('active')
+    $('.page6').css({backgroundImage:"url('../images/store.png')"});
+  }).mouseleave(function(){
+    $(this).find('.hashtag').removeClass('active');
+    $('.page6').css({backgroundImage:"url('')"});
   })
-  }
-  if(buttons.eq(1).hasClass('active')){
-    page5.css({
-      backgroundImage:"url('../images/suite.png')",
-      backgroundRepeat:"no-repeat",
+  $('.promotion .event').mouseenter(function(){
+    $(this).find('.hashtag').addClass('active')
+    $('.page6').css({backgroundImage:"url('../images/event.png')"});
+  }).mouseleave(function(){
+    $(this).find('.hashtag').removeClass('active');
+    $('.page6').css({backgroundImage:"url('../images/store.png')"});
   })
-  }
-    if(buttons.eq(2).hasClass('active')){
-    page5.css({
-      backgroundImage:"url('../images/4dx.png')",
-      backgroundRepeat:"no-repeat",
+  $('.promotion .special').mouseenter(function(){
+    $(this).find('.hashtag').addClass('active')
+    $('.page6').css({backgroundImage:"url('../images/special.png')"});
+  }).mouseleave(function(){
+    $(this).find('.hashtag').removeClass('active');
+    $('.page6').css({backgroundImage:"url('../images/store.png')"});
   })
-  }
-    if(buttons.eq(3).hasClass('active')){
-    page5.css({
-      backgroundImage:"url('../images/chef.png')",
-      backgroundRepeat:"no-repeat",
-  })
-  }
-})
-const page = $('.section.page6')
-
- $('.box2').eq(0).mouseenter(function(){
-   page.css({backgroundRepeat:"no-repeat",backgroundImage:"url('../images/store.png')",transition:".5s"});
- }).mouseleave(function(){
-   page.css({backgroundImage:"url('../images/store.png')"})
- })
-$('.box2').eq(1).mouseenter(function(){
-  page.css({backgroundRepeat:"no-repeat",backgroundImage:"url('../images/event.png')",transition:".5s"});
-})
-$('.box2').eq(2).mouseenter(function(){
-  page.css({backgroundRepeat:"no-repeat",backgroundImage:"url('../images/special.png')",transition:".5s"});
-})
 });
-
+/*
  const itemImg = $('.item') 
  const item = $('.img_box') 
 
@@ -74,3 +34,4 @@ $('.box2').eq(2).mouseenter(function(){
   console.log(idx);
   $(this).find('.lang').text(`${idx+1}`)
  }) 
+*/
