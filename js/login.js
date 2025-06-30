ef40c90273434e3db3bea4c125971843a719e7d7
Ha-im <gungsun1@naver.com>
2025-06-30 21:50:13 +0900

    로그인페이지 수정 및 텝 구현 , 회원가입페이지 완료

--- js/login.js
+++ js/login.js
@@ -1,16 +1,21 @@
-        const tabs = document.querySelectorAll('ul li a');
+        const tabs = document.querySelectorAll('.tabs li a');
         const contents = document.querySelectorAll('div[id^="tab"]');
 
         tabs.forEach(tab => {
             tab.addEventListener('click', function(e) {
-            e.preventDefault();  // 새로고침/페이지 이동 막기
+            e.preventDefault();  
 
             contents.forEach(content => content.style.display = 'none');
-
-            const targetId = this.getAttribute('href').substring(1); // # 제거
+            const targetId = this.getAttribute('href').substring(1); 
             const target = document.getElementById(targetId);
             if (target) target.style.display = 'block';
             });
+            tabs.forEach(function(item, idx){
+            item.addEventListener('click', function(e){
+                tabs.forEach(i => i.classList.remove('active'));
+                tabs[idx].classList.add('active');
+                });
+            })
         });
 
         // 초기 탭 설정 (첫 번째 탭만 보이게)
