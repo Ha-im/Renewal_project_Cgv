245fa9a1e3c965d087ecea9caaf5ca8f81e24d67
Ha-im <gungsun1@naver.com>
2025-07-08 18:07:48 +0900

    푸터부분 개인정보처리방침 클릭 시 모달 뜨게하기 완료

--- js/common.js
+++ js/common.js
@@ -1,16 +1,14 @@
-// header js
-
 
 //footer js
 //모달 띄우기
 $('.footer_link li .policy_li').on('click',function(e){
-    e.preventDefault();
-    $('#policy_modal').css('display','block');
+  e.preventDefault();
+  $('#policy_modal').show();
 })
-  $('#policy_closeModal').on('click',function(){
-    $('#policy_modal').css('display','none');
+$('#policy_closeModal').on('click',function(){
+  $('#policy_modal').hide();
 })
 $('#policy_modal').on('click',function(){
-  $('#policy_modal').css('display','none');
+$('#policy_modal').hide();
 })
 
