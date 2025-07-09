968b4955438ea87e991ee59c3dedcbbd25af99ce
kyt0830 <kyt910830@gmail.com>
2025-07-09 11:09:29 +0900

    주석 제거

--- js/main.js
+++ js/main.js
@@ -10,11 +10,8 @@ new fullpage('#fullpage', {
 const page = $('#fullpage .section'); //fullpage의 전체 페이지를 담는 변수
 let currentPage = 0; //현재 사용자가 보고있는 page (index 체크용)
 let checkEvent = false; //스크롤 이벤트 on/off
-// const langbtn = $('.lang_menu button');
-// const langList = $('#lang_list');
 const menu = $('header'); //header
 const hamBtn = $('.gnb_toggle');
-// const gnb = $('.gnb>li');
 
 
 $(document).on('wheel', function (evt) {
@@ -45,18 +42,7 @@ $(document).on('wheel', function (evt) {
     })
   }
 });
-/*
-gnb.hover(
-  function () {
-    //마우스가 들어오면 할 일
-    $(this).find('.lnb').stop().slideDown(400);
-    langList.slideUp(400);
-  }, function () {
-    //마우스가 나가면 할 일
-    $('.lnb').stop().slideUp(400);
-  }
-);
-*/
+
 
 // header의 ham버튼
 hamBtn.on('click', function () {
@@ -65,12 +51,6 @@ hamBtn.on('click', function () {
   }, 600);
   $(this).fadeOut(200);
 })
-/*
-langbtn.on('click', function () {
- langList.stop().slideToggle(400);
-})
-*/
-
 
 // 메인 배너 js
 
