963b7705af39bf40fd75b7ea47551acd01e5cdb5
yeonion <parkyunme@naver.com>
2025-07-12 23:28:41 +0900

    ticket

--- js/ticket_seat.js
+++ js/ticket_seat.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+  $('.seat').not('.disabled').on('click', function () {
+    $(this).toggleClass('selected');
+  });
+});
 
 
-      
+
+ $.getJSON('./json/ticket_seat.json', function (data) {
+
+  }); 
+
+
