3bcbc893e5826686482b23365509b18865947fd7
Ha-im <gungsun1@naver.com>
2025-07-13 19:43:10 +0900

    sql 최신화

--- sql/signup.sql
+++ sql/signup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 25-07-11 08:13
+-- 생성 시간: 25-07-13 12:41
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -32,6 +32,7 @@ CREATE TABLE `signup_board` (
   `username` varchar(50) NOT NULL,
   `userid` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `regdate` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -40,9 +41,13 @@ CREATE TABLE `signup_board` (
 -- 테이블의 덤프 데이터 `signup_board`
 --
 
-INSERT INTO `signup_board` (`id`, `username`, `userid`, `password`, `email`, `regdate`) VALUES
-(50, '박경선', 'gungsun1', '$2y$10$81UWh6ww7X6M2Sp.xr5rke5g7003k3WFY6KP9qMzNFdcaH5AcSgmC', 'gungsun1@naver.com', '2025-07-11'),
-(51, '박경선', 'gungsun2', '$2y$10$vQQeZ7E0gtVl1JZXeOYFEOa19Qev6qNHoshAU9a8oRzBequbIZPOy', 'gungsun1@naver.com', '2025-07-11');
+INSERT INTO `signup_board` (`id`, `username`, `userid`, `password`, `is_admin`, `email`, `regdate`) VALUES
+(50, '박경선', 'gungsun1', '$2y$10$81UWh6ww7X6M2Sp.xr5rke5g7003k3WFY6KP9qMzNFdcaH5AcSgmC', NULL, 'gungsun1@naver.com', '2025-07-11'),
+(51, '박경선', 'gungsun2', '$2y$10$vQQeZ7E0gtVl1JZXeOYFEOa19Qev6qNHoshAU9a8oRzBequbIZPOy', NULL, 'gungsun1@naver.com', '2025-07-11'),
+(52, '박경선', 'gungsun12', '$2y$10$ZzNPjW5WnncpYAFapBrctOD/gz9XnxexxQbS0CJiNYchKvWWV3Fpu', NULL, 'gungsun1@naver.com', '2025-07-13'),
+(53, '박경선', 'gungsun13', '$2y$10$2Y5aUtR0/s1qsORBlSMe4eEbWzxMCidPv8gD4Aw.vjqyfTr/4Hsta', NULL, 'gungsu1@naver.com', '2025-07-13'),
+(54, '박경선', 'gungsun123', '$2y$10$73ekDuhyWXt4AP9HDOgSXeRZAYTwv63xFrutnHf1RrQOqrx9YTgvy', NULL, 'gungsun1@naver.com', '2025-07-13'),
+(55, '관리자', 'admin', '$2y$10$WvUwp9WiNbutREhOvBmWCuuhMEFcDd4Z3nHjiF1mDBb7hDXAIFiD6', 1, 'gungsun1@naver.com', '2025-07-13');
 
 --
 -- 덤프된 테이블의 인덱스
@@ -63,7 +68,7 @@ ALTER TABLE `signup_board`
 -- 테이블의 AUTO_INCREMENT `signup_board`
 --
 ALTER TABLE `signup_board`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
