d67f83dcdb320212821a0d772f86c6a5fe6ea90d
Ha-im <gungsun1@naver.com>
2025-07-13 17:48:59 +0900

    db연결끊기

--- inc/db.php
+++ inc/db.php
@@ -1,17 +1,17 @@
 <?php
-//mysql 연결코드 절차지향방식
-$host = 'localhost';
-$user = 'signup';
-$password = 'signup1234';
-$dbname = 'signup';
+// //mysql 연결코드 절차지향방식
+// $host = 'localhost';
+// $user = 'signup';
+// $password = 'signup1234';
+// $dbname = 'signup';
 
-// 연결 생성
-$conn = mysqli_connect($host, $user, $password, $dbname);
+// // 연결 생성
+// $conn = mysqli_connect($host, $user, $password, $dbname);
 
-// 연결 확인
-if (!$conn) {
-  die("연결 실패: " . mysqli_connect_error());
-}
+// // 연결 확인
+// if (!$conn) {
+//   die("연결 실패: " . mysqli_connect_error());
+// }
 
-// echo "연결 성공";
+// // echo "연결 성공";
 ?>
