c8254e3ce5e602051303c432b1e900a7f4ebaf33
yeonmi0818 <parkyunme@naver.com>
2025-07-14 11:44:02 +0900

    버튼텍스트 변경

--- view/ticket_seat.view.php
+++ view/ticket_seat.view.php
@@ -334,7 +334,7 @@
           <button type="button" class="prev_page" onclick="location.href='http://localhost/Renewal_project_Cgv/ticket.php'"><span class="material-symbols-outlined">chevron_left</span>이전</button>
 
           <button type="button" class="next_page" onclick="location.href='http://localhost/Renewal_project_Cgv/reservation_confirm.php'">
-            다음
+            결제하기
           </button>
         </div>
       </div>
